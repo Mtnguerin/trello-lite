07d3c7ba1ba7c9283cb99904a0fa884143058837
Martin Guérin <mtnguerin@gmail.com>
2020-05-12 23:18:59 +0200

    Updates

---
+++ precache-manifest.426f09e3ac35475aba5c2ac3279bfada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfe4e38d11561d0333c342545e43aa0",
+    "url": "/trello-lite/index.html"
+  },
+  {
+    "revision": "1c6f3175b00e462deeb9",
+    "url": "/trello-lite/static/css/main.dc3f3ba3.chunk.css"
+  },
+  {
+    "revision": "e1b73f099ca57d31a121",
+    "url": "/trello-lite/static/js/2.e5a77576.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/trello-lite/static/js/2.e5a77576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6f3175b00e462deeb9",
+    "url": "/trello-lite/static/js/main.7dcf4992.chunk.js"
+  },
+  {
+    "revision": "8af28525f15da73af544",
+    "url": "/trello-lite/static/js/runtime-main.ab50dade.js"
+  }
+]);
